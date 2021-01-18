a8f70dbdf5cc5cf4a08b65e5bf720b9401850a86
Luis Henrique <lhff.dev@gmail.com>
2021-01-18 00:34:05 -0300

    Inserida a opção de gerar o relatório de atendimento com a instrução para retirada dos exames

--- frontend/src/app/pages/ordem-de-servico/pages/ordem-de-servico-show.component.ts
+++ frontend/src/app/pages/ordem-de-servico/pages/ordem-de-servico-show.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ApiService } from 'src/app/services/api.service';
 import { ModalAlertComponent } from 'src/app/components/modal-alert/modal-alert.component';
-
+import * as jspdf from 'jspdf';
+import html2canvas from 'html2canvas';
 @Component({
   selector: 'app-ordem-de-servico-show',
   templateUrl: './ordem-de-servico-show.component.html',
@@ -46,5 +47,24 @@ export class OrdemDeServicoShowComponent implements OnInit {
 
   valorTotalExames() {
     return this.ordemDeServico.exames.reduce((sum, exame) => sum + exame.examePreco, 0);
+  }
+
+  gerarRelatorio() {
+    const data = document.getElementById('relatorio');
+
+    html2canvas(data).then(canvas => {
+      const ctx = canvas.getContext('2d');
+      ctx.font = '16px sans-serif';
+      ctx.fillText(
+        'Obs: Para retirar o exame é necessário apresentar o protocolo de atendimento e um documento com foto.',
+        69.9,
+        545 + this.ordemDeServico.exames.length * 32
+      );
+
+      const pdf = new jspdf.jsPDF('p', 'cm', 'a4');
+      const contentDataURL = canvas.toDataURL('image/png');
+      pdf.addImage(contentDataURL, 'PNG', 0.1, 0.1, 20.8, 9 + this.ordemDeServico.exames.length * 0.71);
+      pdf.save('Filename.pdf');
+    });
   }
 }
